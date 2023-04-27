b72c3a5458df7dff1bb0a4f7a00bcadd6a81fe1f
Michael Vars <michaelvars@Michaels-MacBook-Air.local>
2023-04-27 13:35:05 -0500

    form Validated Contact page

---
+++ contact.js
@@ -0,0 +1,31 @@
+// function validateEmail(emailId)
+//     {
+//     
+//     if(emailId.value.match(mailformat))
+//     {
+//     document.form1.text1.focus();
+//     return true;
+//     }
+//     else
+//     {
+//     alert("Invalid email address.");
+//     document.form1.text1.focus();
+//     return false;
+//     }
+//     }
+function validateEmail(email){
+    var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    var firstN = document.getElementById("firstName").value;
+    var lastN = document.getElementById("lastName").value;
+    if(email.value.match(mailformat) && firstN != null &&  firstN != "" && lastN != null && lastN != "" ){
+        alert(`Message Recieved. Thank you for your Correspondence`)
+        return true;
+    }
+    else{
+        alert("Form Incomplete");
+        return false;
+    }
+
+}
+
+
